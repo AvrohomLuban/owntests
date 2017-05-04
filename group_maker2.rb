4fd04dd5b8f85cf27869f0cfbadf307a0b3f19a1
Avraham Luban <lubanavs@gmail.com>
2017-05-04 11:54:53 -0400

    fixed up group_maker2

--- group_maker2.rb
+++ group_maker2.rb
@@ -14,13 +14,13 @@ p students
 students.shuffle!
 
 until students.length == 0
-  
+
   if students.length > 3
     take2 = students.pop(2)
   elsif students.length <= 3
     take2 = students.pop(3)
   end
 
-  students.take2
+  take2
   puts "Group: #{take2.join(",")}"
 end
